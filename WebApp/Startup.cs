919222de90936062ae8300d54058cb5bc284ce45
Mizanur Rahman <mr.milonku@gmail.com>
2021-10-07 16:23:53 +0600

    NSwag added

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using System.Linq;
 using WebApp.Services;
 
 namespace WebApp
@@ -41,6 +44,20 @@ namespace WebApp
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "InvoiceManagementApp API";
+                configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Type into the textbox: Bearer {your JWT token}."
+                });
+
+                configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +67,8 @@ namespace WebApp
             {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
             else
             {
